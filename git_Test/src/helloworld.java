0c3e4e2c65cfe2a68e860532864eefc1c19bb733
pronimance <pronimance@gmail.com>
2012-12-17 17:17:27 +0900

    22424242

--- git_Test/src/helloworld.java
+++ git_Test/src/helloworld.java
@@ -2,22 +2,5 @@
 public class helloworld {
 	public static void main(String[] args) {
 		System.out.println("cc1241241241241241241241");
-		System.out.println("cc2");
-		System.out.println("cc3");
-		System.out.println("cc4");
-		System.out.println("cc5");
-		System.out.println("cc6");
-		
-		System.out.println("cc1");
-		System.out.println("cc2");
-		System.out.println("cc4");
-		System.out.println("cc5");
-		System.out.println("cc6");
-		
-		System.out.println("cc23");
-		System.out.println("cc6666");
-		System.out.println("c77457245602983c");
-		System.out.println("cc");
-		System.out.println("cc");
 	}
 }
